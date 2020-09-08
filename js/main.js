b3d1ac32cf7764dfae6b7e04ebd7425047fe1937
ScoRe187 <belikov.g.c@gmail.com>
2020-09-08 20:32:14 +0300

    Finished, but smt a bit wrong

---
+++ js/main.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+	
+	// var button = document.querySelector('.butn');
+	// var b1 = document.querySelector('.b1');
+	// var b2 = document.querySelector('.b2');
+	// var b3 = document.querySelector('.b3');
+	// var b4 = document.querySelector('.b4');
+	// var b5 = document.querySelector('.b5');
+	// var b6 = document.querySelector('.b6');
+	// var b7 = document.querySelector('.b7');
+	// var dch1 = document.querySelector('.dch1');
+	// var dch2 = document.querySelector('.dch2');
+	// var dch3 = document.querySelector('.dch3');
+	// var dch4 = document.querySelector('.dch4');
+	// var dch5 = document.querySelector('.dch5');
+	// var dch6 = document.querySelector('.dch6');
+	// var dch7 = document.querySelector('.dch7');
+
+	// switch (button) {
+	// 	case b1:
+	// 		b1.addEventListener("click", function(){
+	// 			dch1.classList.toggle('dchactive');
+	// 		});
+	// 		break;
+	// 	case b2:
+	// 		b2.addEventListener("click", function(){
+	// 			dch2.classList.toggle('dchactive');
+	// 		});
+	// 		break;
+	// 	case b3:
+	// 		b3.addEventListener("click", function(){
+	// 			dch3.classList.toggle('dchactive');
+	// 		});
+	// 		break;
+	// 	case b4:
+	// 		b4.addEventListener("click", function(){
+	// 			dch4.classList.toggle('dchactive');
+	// 		});
+	// 		break;
+	// 	case b5:
+	// 		b5.addEventListener("click", function(){
+	// 			dch5.classList.toggle('dchactive');
+	// 		});
+	// 		break;
+	// 	case b6:
+	// 		b6.addEventListener("click", function(){
+	// 			dch6.classList.toggle('dchactive');
+	// 		});
+	// 		break;
+	// 	case b7:
+	// 		b7.addEventListener("click", function(){
+	// 			dch7.classList.toggle('dchactive');
+	// 		});
+	// 		break;
+	// }
+});
